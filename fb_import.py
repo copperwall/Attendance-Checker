88fcd9e1ae2a8fe21023816304023526eb7b7e35
Chris Opperwall <chriso@ifixit.com>
2014-09-06 16:48:03 -0700

    Use config file in import script

--- fb_import.py
+++ fb_import.py
@@ -1,17 +1,16 @@
 import MySQLdb
+import json
 
 # Download Guest List.csv from Facebook event page and copy it to a file named
 # 'list.csv'. Remove the first line (column title) and the '"'s around each
 # name (they cause trouble with MySQL)
 filename = "list.csv"
-data = open(filename, 'r');
-guests = [];
+data = open(filename, 'r')
+guests = []
 
-db_host = "" # Add your host
-db_user = "" # Add your user
-db_password = "" # Add your password
-db_name = "" # Add your database name
-db_table = "" # Add your table name
+# Config Setup
+config_file = open('config.json', 'r')
+config = json.load(config_file)
 
 for line in data:
    person = line.split(',')
@@ -19,7 +18,8 @@ for line in data:
 
 print("Number of guests {}\n".format(str(len(guests))))
 
-db = MySQLdb.connect(db_host, db_user, db_password, db_name)
+db = MySQLdb.connect(config['db_host'], config['db_user'], config['db_password'],
+ config['db_name'])
 cursor = db.cursor()
 
 for person, status in guests:
